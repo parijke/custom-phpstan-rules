a2d7109f677ee91edd87935aa3d5f5b7ff9b1c0f
Paul Rijke <paul@ibuildings.nl>
2024-03-25 12:15:53 +0100

    Create NoEmptyFunctionRule.php

---
+++ src/Architecture/NoEmptyFunctionRule.php
@@ -0,0 +1,35 @@
+<?php
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\ShouldNotHappenException;
+
+class NoEmptyFunctionRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return \PhpParser\Node\Expr\FuncCall::class;
+    }
+
+    /**
+     * @param Node $node
+     * @param Scope $scope
+     * @return string[] errors
+     * @throws ShouldNotHappenException
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        assert($node instanceof Node\Expr\FuncCall);
+
+        if (!$node->name instanceof Node\Name) {
+            throw new ShouldNotHappenException();
+        }
+
+        if ((string) $node->name === 'empty') {
+            return ['Using "empty()" function is not allowed.'];
+        }
+
+        return [];
+    }
+}
