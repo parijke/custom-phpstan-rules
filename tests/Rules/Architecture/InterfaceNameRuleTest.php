0270df388ec754843da3b4c35085f12488791352
Paul Rijke <paul@ibuildings.nl>
2024-03-25 14:09:01 +0100

    Add test

---
+++ tests/Rules/Architecture/InterfaceNameRuleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rules\Architecture;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use Parijke\CustomPhpstanRules\Architecture\InterfaceNameRule;
+
+class InterfaceNameRuleTest extends RuleTestCase
+{
+
+    protected function getRule(): Rule
+    {
+        return new InterfaceNameRule();
+    }
+
+    public function testRule(): void
+    {
+        $this->analyse([__DIR__ . '/data/TestMe.php'], [
+                [
+                    'Interface TestMe should end with "Interface".',
+                    03,
+                ],
+            ]
+        );
+    }
+}
