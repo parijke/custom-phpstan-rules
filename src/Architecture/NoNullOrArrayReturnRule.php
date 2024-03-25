7ce5d5ef5e95c7d25b328c37fe1a698070e2f591
Paul Rijke <paul@ibuildings.nl>
2024-03-25 12:26:00 +0100

    Update NoNullOrArrayReturnRule.php

--- src/Architecture/NoNullOrArrayReturnRule.php
+++ src/Architecture/NoNullOrArrayReturnRule.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Paul\CustomPhpstanRules\Architecture;
+namespace Parijke\CustomPhpstanRules\Architecture;
 
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Return_;
